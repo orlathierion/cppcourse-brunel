b619afa32aed67e1bde1f8b8c85832b9e0ff4aac
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-10-09 15:02:48 +0200

    Rename neuron.hpp to neuron1.hpp

---
+++ neuron1.hpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std ; 
+
+#ifndef NEURON
+#define NEURON
+
+class Neuron {
+	private : 
+	int Ci ; 
+	int Ce ; 
+	double Potential; 
+	vector<double> spikes ;
+	double tau ; 
+	double I ; 
+	double debutI ; 
+	double finI ; 
+	int time ;
+	double MembraneResistance ; 
+	double firingThreshold ; 
+	vector<vector<double> > Record ;
+	double j ; //le potentiel envoyer par un spike 
+	vector<Neuron*> connected ; 
+	
+	public : 
+	Neuron () ;  
+	~Neuron () {}; 
+	int getCi () const ; 
+	int getCe () const ; 
+	double getPotential () const ;
+	double getI () const ;
+	vector<double> getSpikes () const; 
+	int getTime () ;   
+	void setI (double Z ) ; 
+	void setIntervalle (double debut, double fin) ;
+	void setTime (double t) ;
+	void addConnection (Neuron* n ) ; 
+	void RefreshPotential (double h) ;
+	bool Is_spike () ;  
+	vector<vector<double> > getRecord () const ; 
+	void PrintRecord () const ; 
+	void PrintSpike () const ;
+	bool isRefractory () ; 
+	double SendSpikes () ; 
+	double ReceiveSpike () ;  
+	};
+
+#endif 
