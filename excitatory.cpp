cee41a1aa603e4b1415351f9bcb8160a76e7f53c
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-10-30 14:07:10 +0100

    Update excitatory.cpp

--- excitatory.cpp
+++ excitatory.cpp
@@ -1,28 +1,15 @@
 #include <iostream> 
-#include "neuron.hpp"
-#include <array>
+#include "excitatory.hpp" 
 
-using namespace std ; 
+/**
+ * \class excitatory 
+ * this class represents the excitatory neurons of the network. 
+ */
 
-class Excitatory : public Neuron {
-	private : 
-	unsigned int IdentificationNumber ; 
-	
-	public : 
-	Excitatory (unsigned int nb) ;
-	bool IsExcitatory () override ; 
-	unsigned int GetIdentification () const ;
-	void createConnexion () ; 
-	};
-	
 bool Excitatory::IsExcitatory () {
 	return true ; }
 
-Excitatory::Excitatory (unsigned int nb) : 
-Neuron () , 
-IdentificationNumber (nb) {}
+Excitatory::Excitatory () : 
+Neuron () {}
 
-unsigned int Excitatory::GetIdentification () const {
-	return IdentificationNumber ; 
-	}
-void Excitatory::createConnexion () {}
+Excitatory::~Excitatory () {}
