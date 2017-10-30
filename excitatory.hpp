fff70d34d2ae3ada4eb0d43c6aec402d382d6487
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-10-30 14:00:44 +0100

    Create excitatory.hpp

---
+++ excitatory.hpp
@@ -0,0 +1,19 @@
+#ifndef EXCITATORY
+#define EXCITATORY 
+
+#include <iostream> 
+#include "neuron.hpp"
+#include <array>
+
+using namespace std ; 
+
+class Excitatory : public Neuron {
+	private : 
+	
+	public : 
+	Excitatory () ;
+	bool IsExcitatory () override ; 
+	~Excitatory () ; 
+	};
+	
+#endif 
