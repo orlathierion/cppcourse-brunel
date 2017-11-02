0d29815932195efb7de07e49ceb9c52648c497d3
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-11-02 21:58:47 +0100

    Update experiment.cpp

--- experiment.cpp
+++ experiment.cpp
@@ -5,3 +5,13 @@ using namespace std ;
 Experiment::Experiment () :
 n(10000, 2500) 
 {}
+
+Experiment::~Experiment () {} 
+
+void Experiment::refreshNetwork (int h) {
+	n.refreshNetwork (h) ; 
+	}
+
+void Experiment::produceFigure () {
+	n.produceFigure (); 
+	}
