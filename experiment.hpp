0911264da9f809e7dacb9bbadc9821e209ac64f8
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-11-05 16:02:42 +0100

    Update experiment.hpp

--- experiment.hpp
+++ experiment.hpp
@@ -2,7 +2,7 @@
 #define EXPERIMENT
 
 #include <iostream>
-#include "network.hpp" 
+#include "neuron.hpp" 
 #include <random> 
 #include <vector>
  
@@ -10,14 +10,36 @@ using namespace std ;
 
 class Experiment {
 	private : 
-	Network n ; 
-	
-	public :
-	Experiment () ; 
-	~Experiment () ; 
-	void refreshNetwork (int h) ; 
-	void produceFigure (); 
-	};
+	Neuron neuron ; 
+
+	public : 
+	Experiment () ;  
+	~Experiment  (); 
+	int getCi () const ; 
+	int getCe () const ; 
+	double getPotential () const ;
+	double getI () const ;
+	vector<double> getSpikes () const; 
+	int getTime () ; 
+	double getTau () const ; ;  
+	double getMembraneRestistance () const ;  
+	void setI (double Z ) ; 
+	void setIntervalle (double debut, double fin) ;
+	void setTime (double t) ;
+	void setPotential (double t) ; 
+	void addConnection (Neuron* n ) ; 
+	void RefreshPotential (double h) ;
+	bool Is_spike () ;  
+	vector<vector<double> > getRecord () const ; 
+	void PrintRecord () const ; 
+	void PrintSpike () const ;
+	bool isRefractory () ; 
+	double SendSpikes () ; 
+	double ReceiveSpike () ; 
+	void updateRecord () ;  
+	int getNumberSpike () const ; 
+};
+
 
 
 #endif
