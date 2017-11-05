94900097a9b640f31efd07051983578ccf92bc85
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-11-05 16:18:12 +0100

    Update neuron_unittest.cpp

--- neuron_unittest.cpp
+++ neuron_unittest.cpp
@@ -1,27 +1,30 @@
 #include <iostream> 
 #include "neuron.hpp" 
 #include "gtest/gtest.h"
+#include "experiments.hpp" 
 
 using namespace std ; 
 
 /*TEST (NeuronTest, PotentialZEROTest) {
-	Neuron E (true) ;
-	Neuron I (false) ; 
+	Experiment E ; 
+	Experiment I ; 
 	E.setI(0) ; 
 	I.setI (0) ; 
 	E.setIntervalle (0, 20) ; 
 	I.setIntervalle (0, 20) ; 
-	for (unsigned int j (0) ; j<20 ; ++j) {
+	//for (unsigned int j (0) ; j<10 ; ++j) {
+		int j (1) ; 
 		E.setTime (j) ; 
 		I.setTime (j) ;
 		E.RefreshPotential (j) ; 
 		I.RefreshPotential (j) ; 
-	}
+		cout << " neuron unittest 1 21 " << endl ; 
+	//}
 	EXPECT_EQ(E.getNumberSpike(), 0) ;
 	EXPECT_EQ(I.getNumberSpike(), 0) ; 
 	EXPECT_EQ(E.getPotential(), 0.0 ) ; 
 	EXPECT_EQ(I.getPotential(), 0.0 ) ;
-}
+}*/
 
 TEST(NeuronTest, PotentialONETest) {
 	Neuron E (true) ;
@@ -40,21 +43,21 @@ TEST(NeuronTest, PotentialONETest) {
 	EXPECT_EQ(I.getNumberSpike(), 0) ; 
 	EXPECT_DOUBLE_EQ(E.getPotential(), 20.0 ) ; 
 	EXPECT_DOUBLE_EQ(I.getPotential(), 20.0 ) ;
-}*/
+}
 
 TEST(NeuronTest, PotentialTWOTest) {
-	Neuron E (true);
-	Neuron I (false); 
+	Experiment E ; 
+	Experiment I ;  
 	E.setI(1.01) ; 
 	I.setI (1.01) ; 
 	E.setIntervalle (0,20) ; 
 	I.setIntervalle (0,20) ; 
-	for (unsigned int j (0) ; j<20 ; ++j) {
-		E.setTime (j) ; 
-		I.setTime (j) ;
-		E.RefreshPotential (j) ; 
-		I.RefreshPotential (j) ; 
-	}
+	//for (unsigned int j (0) ; j<10 ; ++j) {
+		E.setTime (1) ; 
+		I.setTime (1) ;
+		E.RefreshPotential (1) ; 
+		I.RefreshPotential (1) ; 
+	//}
 	EXPECT_GT(E.getNumberSpike(), 0) ;
 	EXPECT_GT(I.getNumberSpike(), 0) ; 
 }
@@ -64,17 +67,18 @@ TEST(NeuronTest, SpikeBufferTest) {
 	Neuron I (false); 
 	E.addConnection(&I) ; 
 	I.addConnection(&E) ; 
-	E.setI(1.02) ; 
+	E.setI(1.2) ; 
 	E.setIntervalle (0, 20) ; 
 	int time (0) ; 
 	do {
 		E.RefreshPotential (time) ; 
 		I.RefreshPotential (time) ; 
-		++time ; 
-	}while ((not E.Is_spike ()) or (time < 20) ) ;
+		++time ; } while ((E.Is_spike() ) or  not (time < 20 )) ;
+		if (E.Is_spike ()) {
+			cout << "neuron unittest 75  spike buffer" << endl ;}
+ 	I.RefreshPotential (time) ; 
 	I.RefreshPotential (time) ; 
-	I.RefreshPotential (time) ; 
-	EXPECT_GT (I.ReceiveSpike (), 0) ; 
+	EXPECT_NEAR (I.ReceiveSpike (), 0.1, 1.02) ; 
 }
 
 int main (int argc, char **argv) {
