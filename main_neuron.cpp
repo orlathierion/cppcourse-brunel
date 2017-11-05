ea01410e94a3d9d4744db7037629d740aebccc40
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-11-05 16:05:20 +0100

    Update main_neuron.cpp

--- main_neuron.cpp
+++ main_neuron.cpp
@@ -1,18 +1,21 @@
+
 #include <iostream>
 #include <cmath>
 #include "neuron.hpp" 
 #include <string>
 #include <fstream>
 #include "network.hpp"
-#include "experiment.hpp" 
+#include "experiments.hpp" 
 #include "results.txt" 
 
 using namespace std ; 
 
 int main () { 
 	Neuron n (true) ;
-	Network t(3, 2); 
-	//Experiment e; 
-	t.refreshNetwork (1) ;  
+	//Network t(3, 2); 
+	Experiment e; 
+	n.RefreshPotential (1) ; 
+	n.PrintRecord () ;
+	//t.refreshNetwork (1) ;  
 	return 0 ; 
 } 
