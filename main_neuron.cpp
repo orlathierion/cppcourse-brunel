d57c4dc3b14207960633ba812059514806f5fa43
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-11-10 20:00:47 +0100

    Update main_neuron.cpp

--- main_neuron.cpp
+++ main_neuron.cpp
@@ -1,22 +1,28 @@
+
 #include <iostream>
 #include <cmath>
 #include "neuron.hpp" 
 #include <string>
 #include <fstream>
 #include "network.hpp"
-#include "experiments.hpp" 
-#include "results.txt" 
+#include "Spikes.gdb" 
 
 using namespace std ; 
 
 int main () { 
-	//Neuron n (true) ;
-	Network t(10000, 2500); 
-	Experiment e; 
-	for (int i(0); i< 20 ; ++i ) {
-		t.refreshNetwork (i) ; }
-	t.produceFigure () ; 
-	//t.getExcitatory(1)->PrintRecord () ; 
-	cout << "on a fini le pg éain cpp 20 " << endl ; 
+	unsigned int duration ; 
+	double ratio ; 
+	double weight ; 
+	cout << " how long do you want your simulation to be ? (in ms) " << endl ; 
+	cin >> duration ; 
+	duration = duration*10 ; // we multiply by ten to have the right amount of time steps 
+	cout << " what is your Vext/Vthr ratio ? " << endl ; 
+	cin >> ratio ; 
+	cout << " what is the relative inhibitory weight ? " << endl ; 
+	cin >> weight ;
+	Network n (10000, 2500, ratio , weight ) ;
+	for (unsigned int i(0) ; i<duration ; ++i ) {
+		n.refreshNetwork (i) ;}
+	n.produceFigure () ;
 	return 0 ; 
 } 
