ba3e85c53bb7a6bb98a2af2a75148085d9b6349c
orlathierion <32451223+orlathierion@users.noreply.github.com>
2017-11-11 23:43:46 +0100

    Update neuron.hpp

--- neuron.hpp
+++ neuron.hpp
@@ -13,7 +13,7 @@ using namespace std ;
 
 
 /**\class Neuron 
- *  The class Neuron represent a neuron caracterise by its potential, its resistance, 
+ *  The class Neuron represents a neuron caracterised by its potential, its resistance, 
  * the connexion it makes with other neurons. 
  */
 
@@ -41,54 +41,54 @@ class Neuron {
 	
 	public : 
 	
-	/**
-	 * \fn Neuron (bool e, double v, double g, int c)
-	 * \param e : if you want to create qn excitatory neuronm e should be true, otherwise, it should be false 
-	 * \param v : the value of Vext
-	 * \param g : the relqtive weight of inhibitory neuron 
-	 * \param c : the number of excitatory neuron 
-	 */
+/**
+ * \fn Neuron (bool e, double v, double g, int c)
+ * \param e : if you want to create qn excitatory neuronm e should be true, otherwise, it should be false 
+ * \param v : the value of Vext
+ * \param g : the relqtive weight of inhibitory neuron 
+ * \param c : the number of excitatory neuron 
+ */
 	
 	Neuron (bool e, double v, double g, int c) ; 
 	 
 	~Neuron ();  
 
  /*!
-  * \fn double getPotential (unsigned int h) const
-  * \param h : the time qt wich ywe want the potential
-  * \return curent value of the membrqne potential */
+ * \fn double getPotential (unsigned int h) const
+ * \param h : the time qt wich ywe want the potential
+ * \return The curent value of the membrane potential. */
   
 	double getPotential (unsigned int h) const ;
 /*!
  * \fn double getI () const 
- * \return the curent intensity of the external input 
+ * \return The curent intensity of the external input.
  */
 	double getI () const ;
 	
 /*!
  * \fn vector <double> getSpikes() const
- * \return a vector with all the time at wich a spike occured 
+ * \return A vector with all the time at wich a spike occured. 
  */	
 	
 	vector<double> getSpikes () const; 
 
 /*!
  * \fn int getTime () 
- * \return this function give us the time on the cells clock. 
+ * \return This function gives us the time on the cells clock. 
  */	
 	
 	int getTime () ;  
 
 /*!
  * \fn double getFiringThreshold ()  const
- * \return the firing threshold 
+ * \return The firing threshold 
  */	
 	
 	double getFiringThreshold () const ; 
 	
 /*!
  * \fn double getTau () const 
- * \return {this fonction yill return the vqlue of tau	}
+ * \return This fonction will returns the value of tau
  */
 	
 	double getTau () const ; 
@@ -102,16 +102,16 @@ class Neuron {
 	
 /*!
  * \fn void setI (double Z)  
- * \brief this function allow us to change the input coming to the neuron
- * \param Z : intensity of the input
+ * \brief This function allow us to change the input coming to the neuron.
+ * \param Z : intensity of the imput
  */	
 	
 	void setI (double Z ) ; 
 	
 /*!
  * \fn void setIntervalle (double debut, double fin)
- * \brief this fonction will allow us to set the begining and ending time of an external imput
- * \param debut :the begining of the imput
+ * \brief This fonction will allow us to set the begining and ending time of an external imput.
+ * \param debut : the begining of the imput
  * \param fin : the end of the imput
  */ 
 	
@@ -119,7 +119,7 @@ class Neuron {
 	
 	/*! 
  * \fn void setTime(double t )  
- * \brief it allows us to change the inner clock of the cell 
+ * \brief It allows us to change the inner clock of the cell. 
  * \param t : new time
  */
 	
@@ -127,7 +127,7 @@ class Neuron {
 	
 /** 
  * \fn void setPotential (double t )
- * \brief this fonction will allow us to change the value of the emebrane potential. 
+ * \brief This fonction will allow us to change the value of the emebrane potential. 
  * \param t : the new value of the potential
  */
 	
@@ -135,7 +135,7 @@ class Neuron {
 	
 /*!
  * \fn void addConnection (Neuron* n )  
- * \brief addConnection create a new connection with a neuron 
+ * \brief addConnection create a new connection with a neuron. 
  * \param n : it is a pointer on the neuron we want to connect to 
  */	
 	
@@ -144,56 +144,56 @@ class Neuron {
 /*!
  * \fn void RefreshPotential (double h )  
  *  \brief RefreshPotential refresh the membrane potential at time h. 
- * \param h :time in the local clock
+ * \param h : time in the local clock
  */	
 	
 	void RefreshPotential (double h) ;
 	
 /*!
  * \fn bool Is_spike (unsigned int h )
- *  \brief Is_Spike permet de savoir si un spike est crée ou non 
- * \param h .: the time qt wich we want to know if there is a spike
+ *  \brief Is_Spike let us know if there was a spike or not. 
+ * \param h : the time qt wich we want to know if there is a spike
  * \return it will retrun true if a spike is created
  */	
 	
 	bool Is_spike (unsigned int h ) ;  
 	
 /*!
-	 * \fn bool getRecord()
-	 * \brief this fonction will give all the membrane potential at each steps
-	 * \return it will return a table with the time a the corresponding potential membrane
-	 */ 	
+* \fn bool getRecord()
+* \brief this fonction will give all the membrane potential at each steps
+* \return It will return a table with the time a the corresponding potential membrane.
+*/ 	
 	
 	vector<vector<double> > getRecord () const ; 
 	
 /*!
  * \fn int getASpike (unsigned int i) const
  * \param i : the numero of the spike you want to access
- * \return the time at wich this spike occured 
+ * \return The time at wich this spike occured. 
  */
  
  int getASpike (unsigned int i ) const ; 
 	
 /*!
  * \fn bool isRefractory ()
- * \brief this fonction allows us to know if the neuron is in a refractory period 
+ * \brief This fonction allows us to know if the neuron is in a refractory period. 
  * \param i : itcorrespond to the number of the neuron in the network
- *\return it will be true if the membrane is in its refractory periods 
+ *\return It will be true if the membrane is in its refractory periods. 
  */	
 	
 	bool isRefractory ( ) ; 
 	
 /*!
  * \fn bool SendSpikes ()
- * \brief it allow us to know if the neuron is doing a spike at the curent time 
- * \return it will retruns the value of the sended spikes
+ * \brief It allow us to know if the neuron is doing a spike at the curent time. 
+ * \return It will retruns the value of the sended spikes.
  */	
 	
 	double SendSpikes (unsigned int h ) ; 
 	
 /**
  * \fn double ReceiveSpike (unsigned int h )  
- * \brief this fonction will collect all the spikes that the connected neuron have sended 
+ * \brief This fonction will give us the sum of the spikes received by the neuron,and take into account the buffer delay. 
  * \param h : the time at which we look if the spikes were send
  **/ 	
 	
@@ -201,51 +201,51 @@ class Neuron {
 	
 /**
  * \fn bool IsExcitatory ()
- * \brief this fonction allows us to know what type of neuron it is 
- * \return it will be true if the neuron is excitatory
+ * \brief This fonction allows us to know what type of neuron it is. 
+ * \return It will be true if the neuron is excitatory.
  */ 
 	 
 	bool IsExcitatory () ;
 	
 /*!
  * \fn int getCe () const 
- * \return the value of Ce
+ * \return The value of Ce
  */
  
 	int getCe () const ; 
 	
 /*!
  * \fn int getCi () const 
- * \return the value of Ci
+ * \return The value of Ci
  */
 	
 	int getCi () const ; 
 	
 /**
  * \fn  double getNumberSpike () const
- * \return the number of spikes that has occured 
+ * \return The number of spikes that has occured.
  */
 	
 	double getNumberSpike () const ; 
 	
 /**
  * \fn void updateRecord () 
- * \brief this fonction will add the last potential to the record vector 
+ * \brief This fonction will add the last potential to the record vector. 
  */	
 	 
 	void updateRecord () ; 
 	
 /**
  * \fn Neuron* getConnected (int i) 
- * \param i : the nuéber of the neuron we want to qccess in the connected vector
- * \return a pointer on the ith neuron in the connected vector
+ * \param i : The number of the neuron we want to access in the connected vector.
+ * \return A pointer on the ith neuron in the connected vector.
  */
 	
 	Neuron* getConnected (int i ) ; 
 	
 /**
  * \fn int getNumberConnection () const
- * \return the number of connection 
+ * \return The number of connection 
  */
 	
 	int getNumberConnection () const ; 
